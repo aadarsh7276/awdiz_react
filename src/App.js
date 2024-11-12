32d7a41317575eb6496b7c0f688e7604a14ff7da
Aadarsh Yadav <152860259+aadarsh7276@users.noreply.github.com>
2024-11-12 18:29:04 +0530

    updates

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>day 1 React</h1>
+      <h2>Aadarsh Yadav</h2>
+    </div>
+  );
+}
+
+export default App;
